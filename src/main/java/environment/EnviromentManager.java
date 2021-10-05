98ce3b42e6963b19fb3ac6c2105270848a7b6d94
Fonfella <marcello.donzella@gmail.com>
2021-10-05 09:59:43 +0200

    fix

--- src/main/java/environment/EnviromentManager.java
+++ src/main/java/environment/EnviromentManager.java
@@ -1,11 +1,10 @@
 package environment;
 
-import org.openqa.selenium.Platform;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
